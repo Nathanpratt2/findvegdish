331c33525c60a9533cf1584a740450c4d9e4d065
Nathanpratt2 <ntpratt02@gmail.com>
2026-02-08 15:02:19 -0500

    Update fetch_recipes.py

--- fetch_recipes.py
+++ fetch_recipes.py
@@ -89,6 +89,7 @@ DISRUPTORS = [
     ("Veggiekins", "https://veggiekinsblog.com/feed/", ["Easy"],["GF"]), # Will auto-tag GF
     ("ZardyPlants", "https://zardyplants.com/feed/", ["WFPB"]),
     ("Dreena Burton", "https://dreenaburton.com/feed/", ["WFPB"]),
+    ("Holistic Chef Academy", "https://https://holisticchefacademy.com/feed/", []),
     ("Healthy Little Vittles", "https://healthylittlevittles.com/feed/", ["GF"]), # Will auto-tag GF
     ("Healthier Steps", "https://healthiersteps.com/feed/", [])
 ]
